473e517371b5715b591d441e7b762b51758379db
Solomon <rebekrebeloff@gmail.com>
2023-11-19 22:43:33 +0300

    check_balance function added

--- Git 3 scrap.py
+++ Git 3 scrap.py
@@ -7,3 +7,9 @@ def account_creation(base, account_id):
     with a unique account ID and initial balance"""
     base[account_id] = 0.00
     return base
+
+
+def check_balance(base, account_id):
+    """Balance Inquiry: A function to check the current balance
+    of a given account ID."""
+    return base[account_id]
