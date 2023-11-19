672b0fcbec20367e8fad267474c3b7386ffbf3a9
Solomon <rebekrebeloff@gmail.com>
2023-11-19 23:05:29 +0300

    deposit function added

--- Git 3 scrap.py
+++ Git 3 scrap.py
@@ -13,3 +13,11 @@ def check_balance(base, account_id):
     """Balance Inquiry: A function to check the current balance
     of a given account ID."""
     return base[account_id]
+
+def deposit(base, account_id, deposit_input):
+    """Deposit: A function to deposit an amount to a specific account,
+    updating the balance."""
+    if not all(char.isdigit() or char == '.' for char in deposit_input):
+        return "You've entered an incorrect value, try again."
+    base[account_id] += float(deposit_input)
+    return deposit_input
