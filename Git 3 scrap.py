e373db28519ff29323d70f0896d0de2898c65cb9
Solomon <rebekrebeloff@gmail.com>
2023-11-19 22:41:37 +0300

    account_creation function added

--- Git 3 scrap.py
+++ Git 3 scrap.py
@@ -1,2 +1,9 @@
 """Ex3 - Git Commit Mastery"""
 accounts = dict()
+
+
+def account_creation(base, account_id):
+    """Account Creation: A function to create a new account
+    with a unique account ID and initial balance"""
+    base[account_id] = 0.00
+    return base
