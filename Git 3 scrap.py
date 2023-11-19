02f8e518be4232196d55ee76cbd52dab56e50111
Solomon <rebekrebeloff@gmail.com>
2023-11-19 23:12:32 +0300

    account_summary function added

--- Git 3 scrap.py
+++ Git 3 scrap.py
@@ -31,3 +31,10 @@ def withdrawal(base, account_id, withdrawal_input):
         return "You've entered an incorrect value, try again."
     base[account_id] -= float(withdrawal_input)
     return withdrawal_input
+
+
+def account_summary(base, account_id):
+    """Account Summary: A function to print a summary of an account,
+    including the account ID and current balance"""
+    return (f"Your account ID: {account_id} "
+            f"Your current balance: {base[account_id]} $")
